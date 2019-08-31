9b474e6a60176f8af0300b528ca303915e99ff3b
tommasoaldinucci <tommaso.aldinucci@icloud.com>
2019-08-31 21:35:36 +0200

    First commit. Logic and movements are done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import {Ant} from '../ant';
+import {Scenario} from '../scenario';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'langtonAnt';
+  ant: Ant;
+  scenario: Scenario;
+  iteration: number = 0;
+
+  @ViewChild('canvas', { static: true })
+  canvas: ElementRef<HTMLCanvasElement>;
+
+  private ctx: CanvasRenderingContext2D;
+
+  ngOnInit(): void {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    this.ctx.fillStyle = "grey";
+
+    this.ctx.fillRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+    this.scenario = new Scenario(this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+    this.ant = new Ant(500, 300, this.scenario, 5);
+
+  }
+
+  animate(): void {
+    const i = setInterval(() => {
+      this.iteration += 1;
+      this.ant.move();
+      this.ctx.fillStyle = this.ant.currentColor;
+      this.ctx.fillRect(this.ant.currentPosX, this.ant.currentPosY, 5, 5);
+      console.log(this.ant.currentPosX, this.ant.currentPosY);
+    }, 5);
+  }
 }
